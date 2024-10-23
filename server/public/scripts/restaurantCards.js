60691be35873cb3ec7948af98d4fdb48917251cd
student <csharma@sfsu.edu>
2024-10-23 15:09:28 -0700

    minor changes before part 4

--- server/public/scripts/restaurantCards.js
+++ server/public/scripts/restaurantCards.js
@@ -2,12 +2,68 @@
     and js to attach it as a handler per card.
 */
 
-function deleteRestaurantCard(event)
+async function deleteRestaurantCard(event)
 {
     const deleteButton = event.target;
     const restaurantCard = deleteButton.closest('.restaurantItem');
+    const ID = restaurantCard.dataset.id;
+
+    try{
+        const response = await fetch(`/api/restaurants/${ID}`, {
+            method: 'DELETE',
+        });
+
+        if(response.ok)
+        {
+            fetchAndRenderRestaurants();
+        }
+        else{
+            console.error('Failed to delete');
+        }
+    } 
+    catch(error)
+    {
+        console.error('Error: ', error)
+    }
+
     restaurantCard.remove();
 }
+
+async function fetchAndRenderRestaurants(){
+    try{
+        const response = await fetch('/api/restaurants');
+        const restaurants = await response.json();
+    
+        const restaurantContainer = document.querySelector('.restaurantContainer');
+        restaurantContainer.innerHTML = '';
+    
+        restaurants.forEach(restaurant => {
+            const restaurantCard = document.createElement('div');
+            restaurantCard.classList.add('restaurantItem');
+            restaurantCard.dataset.id = restaurant.id;
+    
+            restaurantCard.innerHTML = `
+                <img src="${restaurant.photo}" alt="${restaurant.name}">
+                <div class="restaurantInfo">
+                    <h3>${restaurant.name}</h3>
+                    <p>${restaurant.address}</p>
+                    <p>${restaurant.phone}</p>
+                </div>
+                <button class="deleteButton">X</button>
+            `;
+    
+            restaurantContainer.appendChild(restaurantCard);
+    
+        });
+        deleteListeners();
+    }
+    catch(error)
+    {
+        console.error('Could not fetch restaurants: ', error)
+    }
+
+}
+
 
 function deleteListeners()
 {
@@ -17,4 +73,4 @@ function deleteListeners()
     });
 }
 
-document.addEventListener('DOMContentLoaded', deleteListeners);
+document.addEventListener('DOMContentLoaded', fetchAndRenderRestaurants);
